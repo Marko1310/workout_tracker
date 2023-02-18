fd9c668803005a14ee4bbd05e960ed3bb4bb08bd
Marko1310 <marko.cabo13@gmail.com>
2023-02-18 23:04:27 +0100

    stylizing page

--- root/frontend/src/components/Exercise/Exercise.js
+++ root/frontend/src/components/Exercise/Exercise.js
@@ -3,66 +3,69 @@ import "./Exercise.css";
 const Exercise = () => {
   return (
     <div className="exercise-container">
-      <p>Set</p>
-      <p>Previous</p>
-      <p>kg</p>
-      <p>Reps</p>
+      <p className="exercise-title">Name of the exercise</p>
+      <div className="exercise-grid">
+        <p>Set</p>
+        <p>Previous</p>
+        <p>kg</p>
+        <p>Reps</p>
 
-      {/* <form> */}
-      <p className="set">1</p>
-      <p className="previous">100 kg x 5</p>
-      {/* <label htmlFor="name"></label> */}
-      <input
-        className="forms"
-        type="text"
-        id="kg"
-        name="kg"
-        placeholder="kg"
-      ></input>
-      <input
-        className="forms"
-        type="text"
-        id="reps"
-        name="reps"
-        placeholder="reps"
-      ></input>
+        {/* <form> */}
+        <p className="set">1</p>
+        <p className="previous">100 kg x 5</p>
+        {/* <label htmlFor="name"></label> */}
+        <input
+          className="forms"
+          type="text"
+          id="kg"
+          name="kg"
+          placeholder="kg"
+        ></input>
+        <input
+          className="forms"
+          type="text"
+          id="reps"
+          name="reps"
+          placeholder="reps"
+        ></input>
 
-      <p className="sets">2</p>
-      <p className="previous">100kg x 5</p>
-      {/* <label htmlFor="name"></label> */}
-      <input
-        className="forms"
-        type="text"
-        id="kg"
-        name="kg"
-        placeholder="kg"
-      ></input>
-      <input
-        className="forms"
-        type="text"
-        id="reps"
-        name="reps"
-        placeholder="reps"
-      ></input>
+        <p className="sets">2</p>
+        <p className="previous">100kg x 5</p>
+        {/* <label htmlFor="name"></label> */}
+        <input
+          className="forms"
+          type="text"
+          id="kg"
+          name="kg"
+          placeholder="kg"
+        ></input>
+        <input
+          className="forms"
+          type="text"
+          id="reps"
+          name="reps"
+          placeholder="reps"
+        ></input>
 
-      <p className="sets">3</p>
-      <p className="previous">100kg x 5</p>
-      {/* <label htmlFor="name"></label> */}
-      <input
-        className="forms"
-        type="text"
-        id="kg"
-        name="kg"
-        placeholder="kg"
-      ></input>
-      <input
-        className="forms"
-        type="text"
-        id="reps"
-        name="reps"
-        placeholder="reps"
-      ></input>
-      {/* </form> */}
+        <p className="sets">3</p>
+        <p className="previous">100kg x 5</p>
+        {/* <label htmlFor="name"></label> */}
+        <input
+          className="forms"
+          type="text"
+          id="kg"
+          name="kg"
+          placeholder="kg"
+        ></input>
+        <input
+          className="forms"
+          type="text"
+          id="reps"
+          name="reps"
+          placeholder="reps"
+        ></input>
+        {/* </form> */}
+      </div>
     </div>
   );
 };
