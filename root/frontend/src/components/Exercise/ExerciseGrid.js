e82a9c4298af7789def5d8e63e9548ad7e81c3e9
Marko1310 <marko.cabo13@gmail.com>
2023-02-27 18:27:28 +0100

    stylizing add exercise

--- root/frontend/src/components/Exercise/ExerciseGrid.js
+++ root/frontend/src/components/Exercise/ExerciseGrid.js
@@ -80,9 +80,59 @@ const CardExercise = () => {
           </div>
         </ul>
         {/* ////////////////////////////////////////////////////// */}
+        <ul className="exercise-list-container">
+          <img className="exercise-image" src={logo} alt="exercise"></img>
+          <div className="exercise-card">
+            <li className="exercise-card-title">Day 1 - Upper day: </li>
 
-        <NewExercise />
+            <li> - Bench press</li>
+            <li> - Pullups</li>
+            <li> - Incline dumbell bech</li>
+            <li> - Seated row</li>
+            <li> - Laterall raises</li>
+            <li> - Biceps bench</li>
+            <li> - Triceps rope</li>
+            <li>--------------------------------</li>
+            <li>Date created: 2022-12-20</li>
+          </div>
+        </ul>
+        {/* ////////////////////////////////////////////////////// */}
+        <ul className="exercise-list-container">
+          <img className="exercise-image" src={logo} alt="exercise"></img>
+          <div className="exercise-card">
+            <li className="exercise-card-title">Day 1 - Upper day: </li>
+
+            <li> - Bench press</li>
+            <li> - Pullups</li>
+            <li> - Incline dumbell bech</li>
+            <li> - Seated row</li>
+            <li> - Laterall raises</li>
+            <li> - Biceps bench</li>
+            <li> - Triceps rope</li>
+            <li>--------------------------------</li>
+            <li>Date created: 2022-12-20</li>
+          </div>
+        </ul>
+        {/* ////////////////////////////////////////////////////// */}
+        <ul className="exercise-list-container">
+          <img className="exercise-image" src={logo} alt="exercise"></img>
+          <div className="exercise-card">
+            <li className="exercise-card-title">Day 1 - Upper day: </li>
+
+            <li> - Bench press</li>
+            <li> - Pullups</li>
+            <li> - Incline dumbell bech</li>
+            <li> - Seated row</li>
+            <li> - Laterall raises</li>
+            <li> - Biceps bench</li>
+            <li> - Triceps rope</li>
+            <li>--------------------------------</li>
+            <li>Date created: 2022-12-20</li>
+          </div>
+        </ul>
+        {/* ////////////////////////////////////////////////////// */}
       </div>
+      <NewExercise />
       <AddWorkoutBtn />
     </div>
   );
