b0f8dd4f98a0b7fd3c566448d08f6964ba812afd
Marko1310 <marko.cabo13@gmail.com>
2023-02-27 22:23:09 +0100

    stylizing add workout

--- root/frontend/src/components/Workout/WorkoutGrid.js
+++ root/frontend/src/components/Workout/WorkoutGrid.js
@@ -10,58 +10,64 @@ const CardWorkout = () => {
   const [isModalOpen] = useContext(ModalContext);
 
   return (
-    <>
+    <div className="main-container">
       <div className={`workout-grid ${isModalOpen ? "blurred" : ""}`}>
-        <div className="carDB--container">
-          <ul className="workout-container">
-            <img className="workout-image" src={logo} alt="Workout"></img>
-            <div className="workout-card">
-              <li className="workout-card-title">Workout title</li>
-              <li>4 day split:</li>
-              <li> - Upper day</li>
-              <li> - Lower day</li>
-              <li> - Upper day</li>
-              <li>--------------------------------</li>
-              <li>Date created: 2022-12-20</li>
-            </div>
-          </ul>
-        </div>
+        <ul className="workout-container">
+          <img className="workout-image" src={logo} alt="Workout"></img>
+          <div className="workout-card">
+            <li className="workout-card-title">Workout title</li>
+            <li>4 day split:</li>
+            <li> - Upper day</li>
+            <li> - Lower day</li>
+            <li> - Upper day</li>
+            <li>--------------------------------</li>
+            <li>Date created: 2022-12-20</li>
+          </div>
+        </ul>
         {/* ////////////////////////////////////////////////////// */}
-        <div className="carDB--container">
-          <ul className="workout-container">
-            <img className="workout-image" src={logo} alt="Workout"></img>
-            <div className="workout-card">
-              <li className="workout-card-title">Workout title</li>
-              <li>4 day split:</li>
-              <li> - Upper day</li>
-              <li> - Lower day</li>
-              <li> - Upper day</li>
-              <li>--------------------------------</li>
-              <li>Date created: 2022-12-20</li>
-            </div>
-          </ul>
-        </div>
+        <ul className="workout-container">
+          <img className="workout-image" src={logo} alt="Workout"></img>
+          <div className="workout-card">
+            <li className="workout-card-title">Workout title</li>
+            <li>4 day split:</li>
+            <li> - Upper day</li>
+            <li> - Lower day</li>
+            <li> - Upper day</li>
+            <li>--------------------------------</li>
+            <li>Date created: 2022-12-20</li>
+          </div>
+        </ul>
         {/* ////////////////////////////////////////////////////// */}
-
-        <div className="carDB--container">
-          <ul className="workout-container">
-            <img className="workout-image" src={logo} alt="Workout"></img>
-            <div className="workout-card">
-              <li className="workout-card-title">Workout title</li>
-              <li>4 day split:</li>
-              <li> - Upper day</li>
-              <li> - Lower day</li>
-              <li> - Upper day</li>
-              <li>--------------------------------</li>
-              <li>Date created: 2022-12-20</li>
-            </div>
-          </ul>
-        </div>
+        <ul className="workout-container">
+          <img className="workout-image" src={logo} alt="Workout"></img>
+          <div className="workout-card">
+            <li className="workout-card-title">Workout title</li>
+            <li>4 day split:</li>
+            <li> - Upper day</li>
+            <li> - Lower day</li>
+            <li> - Upper day</li>
+            <li>--------------------------------</li>
+            <li>Date created: 2022-12-20</li>
+          </div>
+        </ul>
+        {/* ////////////////////////////////////////////////////// */}
+        <ul className="workout-container">
+          <img className="workout-image" src={logo} alt="Workout"></img>
+          <div className="workout-card">
+            <li className="workout-card-title">Workout title</li>
+            <li>4 day split:</li>
+            <li> - Upper day</li>
+            <li> - Lower day</li>
+            <li> - Upper day</li>
+            <li>--------------------------------</li>
+            <li>Date created: 2022-12-20</li>
+          </div>
+        </ul>
         {/* ////////////////////////////////////////////////////// */}
       </div>
-      <AddWorkoutBtn />
       <NewWorkout />
-    </>
+      <AddWorkoutBtn />
+    </div>
   );
 };
 
