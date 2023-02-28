8689dfdd0ca935b038cc34573b847aabe4fd4684
Marko1310 <marko.cabo13@gmail.com>
2023-02-28 23:19:23 +0100

    stylizing workout

--- root/frontend/src/components/Workouts/Exercise.js
+++ root/frontend/src/components/Workouts/Exercise.js
@@ -3,7 +3,10 @@ import "./Exercise.css";
 const Exercise = () => {
   return (
     <div className="exercise-container">
-      <p className="exercise-title">Name of the exercise</p>
+      <div className="title-container">
+        <p className="exercise-title">Name of the exercise</p>
+        <div>...</div>
+      </div>
       <div className="exercise">
         <p>Set</p>
         <p>Previous</p>
@@ -64,8 +67,27 @@ const Exercise = () => {
           name="reps"
           placeholder="reps"
         ></input>
+
+        <p className="set">3</p>
+        <p className="previous">100kg x 5</p>
+        {/* <label htmlFor="name"></label> */}
+        <input
+          className="exercise-forms"
+          type="text"
+          id="kg"
+          name="kg"
+          placeholder="kg"
+        ></input>
+        <input
+          className="exercise-forms"
+          type="text"
+          id="reps"
+          name="reps"
+          placeholder="reps"
+        ></input>
         {/* </form> */}
       </div>
+      <button className="addSetBtn">+ Add Set</button>
     </div>
   );
 };
