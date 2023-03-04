61f44546b32e64b22adb413576c41313c61b0c8c
Marko1310 <marko.cabo13@gmail.com>
2023-03-04 16:20:49 +0100

    login security backend

--- root/backend/controllers/login.js
+++ root/backend/controllers/login.js
@@ -4,29 +4,26 @@ const handleLogin = (req, res, pool, bcrypt) => {
   const user = pool
     .query("SELECT * FROM users WHERE email = $1", [email])
     .then((user) => {
+      // if user exists
+      if (user.rows.length === 0) {
+        return res.json("Something is wrong with your credentials");
+      }
+
+      // is password is valid
+      const isValid = bcrypt.compareSync(password, user.rows[0].password);
+      if (!isValid) {
+        return res.json("Something is wrong with your credentials");
+      }
+
+      // return everything except the password
       userCredentials = {
         id: user.rows[0].id,
         name: user.rows[0].name,
         email: user.rows[0].email,
       };
       res.json(userCredentials);
-    });
-
-  // pool
-  //   .query("SELECT * FROM users WHERE email = $1", [email])
-  //   .then((data) => {
-  //     const isValid = bcrypt.compareSync(password, data.rows[0].password);
-  //     if (isValid) {
-  //       pool
-  //         .query("SELECT * FROM users WHERE email = $1", [email])
-  //         .then((user) => res.json(user.rows[0]));
-  //     } else {
-  //       res.status(400).json("Something is wrong with your credentials");
-  //     }
-  //   })
-  //   .catch(() =>
-  //     res.status(400).json("Something is wrong with your credentials")
-  //   );
+    })
+    .catch(() => res.status(500).send(err.message));
 };
 
 module.exports = {
