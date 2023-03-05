817d79199ec471a7084bcfc76810ca56ed4fbc70
Marko1310 <marko.cabo13@gmail.com>
2023-03-05 18:26:44 +0100

    adding and retrieving new splits

--- root/backend/routes/Workouts.js
+++ root/backend/routes/Workouts.js
@@ -12,13 +12,8 @@ const pool = new Pool({
   //   ssl: true,
 });
 
-// @route   GET /api/splits/test
-// @desc    Test the splits route
-// @access  Public
-router.get("/test", (req, res) => {
-  res.json("Test working");
-});
-
+//      ADDING DATA     //
+///////////////////////////////
 // @route   POST /api/splits/new
 // @desc    Create new split
 // @access  Private
@@ -27,6 +22,16 @@ router.post("/split/new", requiresAuth, async (req, res) => {
     user_id = req.user.id;
     const date = new Date();
     const { title, days } = req.body;
+
+    if (!title) {
+      return res.status(400).json({ title: "Title field can not be empty" });
+    }
+
+    if (days === 0 || days < 1 || days > 7) {
+      return res
+        .status(400)
+        .json({ days: "Days field should be between 1 and 7" });
+    }
 
     const split = await pool.query(
       "INSERT INTO splits (split_name, user_id, days, date) VALUES ($1, $2, $3, $4) RETURNING *",
@@ -38,6 +43,8 @@ router.post("/split/new", requiresAuth, async (req, res) => {
   }
 });
 
+//      RETRIEVING DATA     //
+///////////////////////////////
 // @route   GET /api/splits/current
 // @desc    get user splita
 // @access  Private
