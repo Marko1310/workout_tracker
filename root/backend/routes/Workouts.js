912ab9b04bf2cec07c4c60a6cff5068657cc06e6
Marko1310 <marko.cabo13@gmail.com>
2023-03-05 16:42:36 +0100

    adding a new split route

--- root/backend/routes/Workouts.js
+++ root/backend/routes/Workouts.js
@@ -15,9 +15,26 @@ const pool = new Pool({
 // @route   GET /api/splits/test
 // @desc    Test the splits route
 // @access  Public
-
 router.get("/test", (req, res) => {
   res.json("Test working");
+});
+
+// @route   POST /api/splits/new
+// @desc    Create new split
+// @access  Private
+router.post("/split/new", requiresAuth, async (req, res) => {
+  try {
+    user_id = req.user.id;
+    const { title, days } = req.body;
+
+    const split = await pool.query(
+      "INSERT INTO splits (split_name, user_id, days) VALUES ($1, $2, $3) RETURNING *",
+      [title, user_id, days]
+    );
+    res.json(split.rows[0]);
+  } catch {
+    return res.status(500).send(err.message);
+  }
 });
 
 module.exports = router;
