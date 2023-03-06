22183b2c5faaeb44cc31a6559e37aa3dea9b8173
Marko1310 <marko.cabo13@gmail.com>
2023-03-06 14:10:13 +0100

    setting up retrieving data routes

--- root/backend/routes/Workouts.js
+++ root/backend/routes/Workouts.js
@@ -196,14 +196,14 @@ router.get(
   async (req, res) => {
     try {
       user_id = req.user.id;
-      const { workout_id, exercise_id } = req.body;
+      const { workout_id } = req.body;
 
       // Get user exercises from the workout
       const exercises = await pool.query(
         "SELECT * FROM exercises WHERE user_id=$1 AND workout_id = $2 ORDER BY date DESC",
         [user_id, workout_id]
       );
-      console.log(exercises.rows);
+      //   console.log(exercises.rows);
 
       // Get track data from each exercise from the workout
       // 1. get all the id's of the exercises from the given workout
@@ -213,12 +213,28 @@ router.get(
       //   );
       //   console.log(exercise_id.rows);
 
-      const track = await pool.query(
-        "SELECT * FROM track WHERE user_id=$1 AND exercise_id = $2 ORDER BY date DESC",
-        [user_id, exercise_id]
+      //   const track = await pool.query(
+      //     "SELECT * FROM track WHERE user_id=$1 AND exercise_id = $2 ORDER BY date DESC",
+      //     [user_id, exercise_id]
+      //   );
+
+      //   const track = await pool.query(
+      //     "SELECT * FROM track WHERE user_id=$1 ORDER BY date DESC",
+      //     [user_id]
+      //   );
+
+      const track_data = await pool.query(
+        "SELECT w.workout_name, e.exercise_name, t.set, t.weight FROM workouts w INNER JOIN exercises e ON e.workout_id = w.workout_id INNER JOIN track t ON t.exercise_id = e.exercise_id WHERE w.workout_id = $1;",
+        [9]
       );
 
-      res.json({ exercises: exercises.rows, track: track.rows });
+      //   const track_data = await pool.query(
+      //     "SELECT w.workout_name, e.exercise_name FROM workouts w INNER JOIN exercises e ON e.workout_id = w.workout_id WHERE w.workout_id = $1;",
+      //     [9]
+      //   );
+
+      console.log(track_data.rows);
+      res.json(track_data.rows);
     } catch (err) {
       return res.status(500).send(err.message);
     }
