54844decf8e4447f09a56e32b55002ffc7ee0bdd
Marko1310 <marko.cabo13@gmail.com>
2023-03-07 18:29:39 +0100

    retrieving track data for each exercise

--- root/frontend/src/components/Workouts/Exercise.js
+++ root/frontend/src/components/Workouts/Exercise.js
@@ -7,41 +7,41 @@ const Exercise = ({ el }) => {
         <p className="exercise-title">{el.exercise_name}</p>
         <div>...</div>
       </div>
-      <div className="exercise">
+      <div className="exercise-navbar">
         <p>Set</p>
         <p>Previous</p>
         <p>kg</p>
         <p>Reps</p>
-
-        {/* <form> */}
-        {el.sets_reps_weigh.map((el) => {
-          return (
-            <>
-              <p className="set">{el.sets}</p>
-              <p className="previous">
-                {el.weight} kg x {el.reps}
-              </p>
-              {/* <label htmlFor="name"></label> */}
-              <input
-                className="exercise-forms"
-                type="text"
-                id="kg"
-                name="kg"
-                placeholder="kg"
-              ></input>
-              <input
-                className="exercise-forms"
-                type="text"
-                id="reps"
-                name="reps"
-                placeholder="reps"
-              ></input>
-            </>
-          );
-        })}
-
-        {/* </form> */}
       </div>
+
+      {/* <form> */}
+      {el.sets_reps_weigh.map((el) => {
+        return (
+          <div className="exercise">
+            <p className="set">{el.sets}</p>
+            <p className="previous">
+              {el.weight} kg x {el.reps}
+            </p>
+            {/* <label htmlFor="name"></label> */}
+            <input
+              className="exercise-forms"
+              type="text"
+              id="kg"
+              name="kg"
+              placeholder="kg"
+            ></input>
+            <input
+              className="exercise-forms"
+              type="text"
+              id="reps"
+              name="reps"
+              placeholder="reps"
+            ></input>
+          </div>
+        );
+      })}
+
+      {/* </form> */}
       <button className="addSetBtn">+ Add Set</button>
     </div>
   );
