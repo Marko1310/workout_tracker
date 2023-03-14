a41444ebfdc5fa65660dc9ec35e0977439077429
Marko1310 <marko.cabo13@gmail.com>
2023-03-14 17:56:51 +0100

    bug fix

--- root/backend/routes/Auth.js
+++ root/backend/routes/Auth.js
@@ -66,6 +66,7 @@ router.post("/register", async (req, res) => {
       expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
       httpOnly: false,
       secure: true,
+      sameSite: "none",
     });
 
     const userCredentials = {
